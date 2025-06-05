946b330a4287b6dc2f36aacad4917497006e06e4
Carlos Torres <j.carlos.torres@me.com>
2025-06-04 21:44:06 -0700

    Change default port from 8080 to 8000 and add configurable address/port (#5)
    * Change default port from 8080 to 8000 and add configurable address/port
    
    * Change argument name from ip to address

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
 use api::services::services_routes;
 use axum::Router;
+use clap::Parser;
 
 mod api;
 mod model;
 mod registry;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value = "0.0.0.0")]
+    address: String,
+
+    #[arg(short, long, default_value_t = 8000)]
+    port: u16,
+}
+
 #[tokio::main]
 async fn main() {
+    let args = Args::parse();
     let app = create_app();
-    let listener = match tokio::net::TcpListener::bind("0.0.0.0:8080").await {
+    let bind_address = format!("{}:{}", args.address, args.port);
+
+    let listener = match tokio::net::TcpListener::bind(&bind_address).await {
         Ok(listener) => listener,
         Err(e) => {
-            eprintln!("Failed to bind to address 0.0.0.0:8080: {}", e);
+            eprintln!("Failed to bind to address {}: {}", bind_address, e);
             std::process::exit(1);
         }
     };
-    println!("Starting Xolotl on 0.0.0.0:8080");
+    println!("Starting Xolotl on {}", bind_address);
     axum::serve(listener, app).await.unwrap();
 }
 
